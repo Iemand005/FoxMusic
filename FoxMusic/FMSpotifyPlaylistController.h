72f8e43c45557861e97551859f2eb4311bef2330
Lasse <sprat.com>
2024-07-02 02:16:38 +0200

    turn it up faster take your tame and make ut tip after

---
+++ FoxMusic/FMSpotifyPlaylistController.h
@@ -0,0 +1,13 @@
+//
+//  FMSpotifyPlaylistController.h
+//  FoxMusic
+//
+//  Created by Lasse Lauwerys on 2/07/24.
+//  Copyright (c) 2024 Lasse Lauwerys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMSpotifyPlaylistController : NSObject <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ FoxMusic/FMSpotifyPlaylistController.m
@@ -0,0 +1,15 @@
+//
+//  FMSpotifyPlaylistController.m
+//  FoxMusic
+//
+//  Created by Lasse Lauwerys on 2/07/24.
+//  Copyright (c) 2024 Lasse Lauwerys. All rights reserved.
+//
+
+#import "FMSpotifyPlaylistController.h"
+
+@implementation FMSpotifyPlaylistController
+
+- 
+
+@end
